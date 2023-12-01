5848bd2d794c668fa05b1e73a9ee07f18a84afef
250T4 <144907705+250T4@users.noreply.github.com>
2023-12-01 16:07:45 +0300

    initial commit

--- app/src/main/java/com/example/pizzarecipes/MainActivity.java
+++ app/src/main/java/com/example/pizzarecipes/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pizzarecipes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -22,5 +23,40 @@ public class MainActivity extends AppCompatActivity {
 
         pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza1,
                 Utils.PIZZA_1_TITLE, Utils.PIZZA_1_DESCRIPTION, Utils.PIZZA_1_RECIPE));
+
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza1,
+                Utils.PIZZA_2_TITLE, Utils.PIZZA_2_DESCRIPTION, Utils.PIZZA_2_RECIPE));
+
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza1,
+                Utils.PIZZA_3_TITLE, Utils.PIZZA_3_DESCRIPTION, Utils.PIZZA_3_RECIPE));
+
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza1,
+                Utils.PIZZA_4_TITLE, Utils.PIZZA_4_DESCRIPTION, Utils.PIZZA_4_RECIPE));
+
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza1,
+                Utils.PIZZA_5_TITLE, Utils.PIZZA_5_DESCRIPTION, Utils.PIZZA_5_RECIPE));
+
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza1,
+                Utils.PIZZA_6_TITLE, Utils.PIZZA_6_DESCRIPTION, Utils.PIZZA_6_RECIPE));
+
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza1,
+                Utils.PIZZA_7_TITLE, Utils.PIZZA_7_DESCRIPTION, Utils.PIZZA_7_RECIPE));
+
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza1,
+                Utils.PIZZA_8_TITLE, Utils.PIZZA_8_DESCRIPTION, Utils.PIZZA_8_RECIPE));
+
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza1,
+                Utils.PIZZA_9_TITLE, Utils.PIZZA_9_DESCRIPTION, Utils.PIZZA_9_RECIPE));
+
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza1,
+                Utils.PIZZA_10_TITLE, Utils.PIZZA_10_DESCRIPTION, Utils.PIZZA_10_RECIPE));
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        adapter = new PizzaRecipeAdapter(pizzaRecipeItems);
+        layoutManager = new LinearLayoutManager(this);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
--- app/src/main/java/com/example/pizzarecipes/Utils.java
+++ app/src/main/java/com/example/pizzarecipes/Utils.java
@@ -26,5 +26,239 @@ public class Utils {
             "To finish\n" +
             "\n" +
             "    handful of basil leaves (optional)";
+
+    public static final String PIZZA_2_TITLE = "Pizza Margherita in 4 easy steps";
+    public static final String PIZZA_2_DESCRIPTION = "Even a novice cook can master the art of perfect pizza with our step-by-step guide. This homemade pizza recipe features a simple pizza dough and toppings.";
+    public static final String PIZZA_2_RECIPE = "Ingredients\n" +
+            "For the base\n" +
+            "\n" +
+            "    300g strong bread flour\n" +
+            "    1 tsp instant yeast (from a sachet or a tub)\n" +
+            "    1 tsp salt\n" +
+            "    1 tbsp olive oil, plus extra for drizzling\n" +
+            "\n" +
+            "For the tomato sauce\n" +
+            "\n" +
+            "    100ml passata\n" +
+            "    handful fresh basil or 1 tsp dried\n" +
+            "    1 garlic clove, crushed\n" +
+            "\n" +
+            "For the topping\n" +
+            "\n" +
+            "    125g ball mozzarella, sliced\n" +
+            "    handful grated or shaved parmesan (or vegetarian alternative)\n" +
+            "    handful of cherry tomatoes, halved \n" +
+            "\n" +
+            "To finish\n" +
+            "\n" +
+            "    handful of basil leaves (optional)";
+
+    public static final String PIZZA_3_TITLE = "Pizza Margherita in 4 easy steps";
+    public static final String PIZZA_3_DESCRIPTION = "Even a novice cook can master the art of perfect pizza with our step-by-step guide. This homemade pizza recipe features a simple pizza dough and toppings.";
+    public static final String PIZZA_3_RECIPE = "Ingredients\n" +
+            "For the base\n" +
+            "\n" +
+            "    300g strong bread flour\n" +
+            "    1 tsp instant yeast (from a sachet or a tub)\n" +
+            "    1 tsp salt\n" +
+            "    1 tbsp olive oil, plus extra for drizzling\n" +
+            "\n" +
+            "For the tomato sauce\n" +
+            "\n" +
+            "    100ml passata\n" +
+            "    handful fresh basil or 1 tsp dried\n" +
+            "    1 garlic clove, crushed\n" +
+            "\n" +
+            "For the topping\n" +
+            "\n" +
+            "    125g ball mozzarella, sliced\n" +
+            "    handful grated or shaved parmesan (or vegetarian alternative)\n" +
+            "    handful of cherry tomatoes, halved \n" +
+            "\n" +
+            "To finish\n" +
+            "\n" +
+            "    handful of basil leaves (optional)";
+
+    public static final String PIZZA_4_TITLE = "Pizza Margherita in 4 easy steps";
+    public static final String PIZZA_4_DESCRIPTION = "Even a novice cook can master the art of perfect pizza with our step-by-step guide. This homemade pizza recipe features a simple pizza dough and toppings.";
+    public static final String PIZZA_4_RECIPE = "Ingredients\n" +
+            "For the base\n" +
+            "\n" +
+            "    300g strong bread flour\n" +
+            "    1 tsp instant yeast (from a sachet or a tub)\n" +
+            "    1 tsp salt\n" +
+            "    1 tbsp olive oil, plus extra for drizzling\n" +
+            "\n" +
+            "For the tomato sauce\n" +
+            "\n" +
+            "    100ml passata\n" +
+            "    handful fresh basil or 1 tsp dried\n" +
+            "    1 garlic clove, crushed\n" +
+            "\n" +
+            "For the topping\n" +
+            "\n" +
+            "    125g ball mozzarella, sliced\n" +
+            "    handful grated or shaved parmesan (or vegetarian alternative)\n" +
+            "    handful of cherry tomatoes, halved \n" +
+            "\n" +
+            "To finish\n" +
+            "\n" +
+            "    handful of basil leaves (optional)";
+
+    public static final String PIZZA_5_TITLE = "Pizza Margherita in 4 easy steps";
+    public static final String PIZZA_5_DESCRIPTION = "Even a novice cook can master the art of perfect pizza with our step-by-step guide. This homemade pizza recipe features a simple pizza dough and toppings.";
+    public static final String PIZZA_5_RECIPE = "Ingredients\n" +
+            "For the base\n" +
+            "\n" +
+            "    300g strong bread flour\n" +
+            "    1 tsp instant yeast (from a sachet or a tub)\n" +
+            "    1 tsp salt\n" +
+            "    1 tbsp olive oil, plus extra for drizzling\n" +
+            "\n" +
+            "For the tomato sauce\n" +
+            "\n" +
+            "    100ml passata\n" +
+            "    handful fresh basil or 1 tsp dried\n" +
+            "    1 garlic clove, crushed\n" +
+            "\n" +
+            "For the topping\n" +
+            "\n" +
+            "    125g ball mozzarella, sliced\n" +
+            "    handful grated or shaved parmesan (or vegetarian alternative)\n" +
+            "    handful of cherry tomatoes, halved \n" +
+            "\n" +
+            "To finish\n" +
+            "\n" +
+            "    handful of basil leaves (optional)";
+
+    public static final String PIZZA_6_TITLE = "Pizza Margherita in 4 easy steps";
+    public static final String PIZZA_6_DESCRIPTION = "Even a novice cook can master the art of perfect pizza with our step-by-step guide. This homemade pizza recipe features a simple pizza dough and toppings.";
+    public static final String PIZZA_6_RECIPE = "Ingredients\n" +
+            "For the base\n" +
+            "\n" +
+            "    300g strong bread flour\n" +
+            "    1 tsp instant yeast (from a sachet or a tub)\n" +
+            "    1 tsp salt\n" +
+            "    1 tbsp olive oil, plus extra for drizzling\n" +
+            "\n" +
+            "For the tomato sauce\n" +
+            "\n" +
+            "    100ml passata\n" +
+            "    handful fresh basil or 1 tsp dried\n" +
+            "    1 garlic clove, crushed\n" +
+            "\n" +
+            "For the topping\n" +
+            "\n" +
+            "    125g ball mozzarella, sliced\n" +
+            "    handful grated or shaved parmesan (or vegetarian alternative)\n" +
+            "    handful of cherry tomatoes, halved \n" +
+            "\n" +
+            "To finish\n" +
+            "\n" +
+            "    handful of basil leaves (optional)";
+
+    public static final String PIZZA_7_TITLE = "Pizza Margherita in 4 easy steps";
+    public static final String PIZZA_7_DESCRIPTION = "Even a novice cook can master the art of perfect pizza with our step-by-step guide. This homemade pizza recipe features a simple pizza dough and toppings.";
+    public static final String PIZZA_7_RECIPE = "Ingredients\n" +
+            "For the base\n" +
+            "\n" +
+            "    300g strong bread flour\n" +
+            "    1 tsp instant yeast (from a sachet or a tub)\n" +
+            "    1 tsp salt\n" +
+            "    1 tbsp olive oil, plus extra for drizzling\n" +
+            "\n" +
+            "For the tomato sauce\n" +
+            "\n" +
+            "    100ml passata\n" +
+            "    handful fresh basil or 1 tsp dried\n" +
+            "    1 garlic clove, crushed\n" +
+            "\n" +
+            "For the topping\n" +
+            "\n" +
+            "    125g ball mozzarella, sliced\n" +
+            "    handful grated or shaved parmesan (or vegetarian alternative)\n" +
+            "    handful of cherry tomatoes, halved \n" +
+            "\n" +
+            "To finish\n" +
+            "\n" +
+            "    handful of basil leaves (optional)";
+
+    public static final String PIZZA_8_TITLE = "Pizza Margherita in 4 easy steps";
+    public static final String PIZZA_8_DESCRIPTION = "Even a novice cook can master the art of perfect pizza with our step-by-step guide. This homemade pizza recipe features a simple pizza dough and toppings.";
+    public static final String PIZZA_8_RECIPE = "Ingredients\n" +
+            "For the base\n" +
+            "\n" +
+            "    300g strong bread flour\n" +
+            "    1 tsp instant yeast (from a sachet or a tub)\n" +
+            "    1 tsp salt\n" +
+            "    1 tbsp olive oil, plus extra for drizzling\n" +
+            "\n" +
+            "For the tomato sauce\n" +
+            "\n" +
+            "    100ml passata\n" +
+            "    handful fresh basil or 1 tsp dried\n" +
+            "    1 garlic clove, crushed\n" +
+            "\n" +
+            "For the topping\n" +
+            "\n" +
+            "    125g ball mozzarella, sliced\n" +
+            "    handful grated or shaved parmesan (or vegetarian alternative)\n" +
+            "    handful of cherry tomatoes, halved \n" +
+            "\n" +
+            "To finish\n" +
+            "\n" +
+            "    handful of basil leaves (optional)";
+
+    public static final String PIZZA_9_TITLE = "Pizza Margherita in 4 easy steps";
+    public static final String PIZZA_9_DESCRIPTION = "Even a novice cook can master the art of perfect pizza with our step-by-step guide. This homemade pizza recipe features a simple pizza dough and toppings.";
+    public static final String PIZZA_9_RECIPE = "Ingredients\n" +
+            "For the base\n" +
+            "\n" +
+            "    300g strong bread flour\n" +
+            "    1 tsp instant yeast (from a sachet or a tub)\n" +
+            "    1 tsp salt\n" +
+            "    1 tbsp olive oil, plus extra for drizzling\n" +
+            "\n" +
+            "For the tomato sauce\n" +
+            "\n" +
+            "    100ml passata\n" +
+            "    handful fresh basil or 1 tsp dried\n" +
+            "    1 garlic clove, crushed\n" +
+            "\n" +
+            "For the topping\n" +
+            "\n" +
+            "    125g ball mozzarella, sliced\n" +
+            "    handful grated or shaved parmesan (or vegetarian alternative)\n" +
+            "    handful of cherry tomatoes, halved \n" +
+            "\n" +
+            "To finish\n" +
+            "\n" +
+            "    handful of basil leaves (optional)";
+
+    public static final String PIZZA_10_TITLE = "Pizza Margherita in 4 easy steps";
+    public static final String PIZZA_10_DESCRIPTION = "Even a novice cook can master the art of perfect pizza with our step-by-step guide. This homemade pizza recipe features a simple pizza dough and toppings.";
+    public static final String PIZZA_10_RECIPE = "Ingredients\n" +
+            "For the base\n" +
+            "\n" +
+            "    300g strong bread flour\n" +
+            "    1 tsp instant yeast (from a sachet or a tub)\n" +
+            "    1 tsp salt\n" +
+            "    1 tbsp olive oil, plus extra for drizzling\n" +
+            "\n" +
+            "For the tomato sauce\n" +
+            "\n" +
+            "    100ml passata\n" +
+            "    handful fresh basil or 1 tsp dried\n" +
+            "    1 garlic clove, crushed\n" +
+            "\n" +
+            "For the topping\n" +
+            "\n" +
+            "    125g ball mozzarella, sliced\n" +
+            "    handful grated or shaved parmesan (or vegetarian alternative)\n" +
+            "    handful of cherry tomatoes, halved \n" +
+            "\n" +
+            "To finish\n" +
+            "\n" +
+            "    handful of basil leaves (optional)";
 }
 
